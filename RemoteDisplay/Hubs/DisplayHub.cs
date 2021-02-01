181a0c604f5c7b3e0bb2b28dfdd75550dd11c23e
Steve Morgan <steve@wormpurple.com>
2021-02-01 15:49:49 +0000

    Add hub methods for keyboard

--- RemoteDisplay/Hubs/DisplayHub.cs
+++ RemoteDisplay/Hubs/DisplayHub.cs
@@ -10,15 +10,29 @@ namespace RemoteDisplay.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
-
         public async Task SetMode(DisplayMode mode)
         {
             await Clients.All.SendAsync("SetMode", mode.Width, mode.Height);
         }
-
         public async Task Write(ushort offset, byte value)
         {
             await Clients.All.SendAsync("Write", offset, value);
+        }
+        public async Task KeyDown(byte code)
+        {
+            await Clients.All.SendAsync("KeyDown", code);
+        }
+        public async Task KeyUp(byte code)
+        {
+            await Clients.All.SendAsync("KeyDown", code);
+        }
+        public async Task RequestStatus()
+        {
+            await Clients.All.SendAsync("RequestStatus");
+        }
+        public async Task Clear()
+        {
+            await Clients.All.SendAsync("Clear");
         }
     }
 }
